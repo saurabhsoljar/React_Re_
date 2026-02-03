b1f9687d0dfa69a50faec6a06744f45b608cc99d
saurabh kumar <93548806+saurabhsoljar@users.noreply.github.com>
2026-02-03 23:29:37 +0530

    custom react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,8 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App! </h1>
+    </div>
+  )
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MyApp />
   </StrictMode>,
 )
